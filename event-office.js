d2a0e7f0a38113f0b090b0f9344b0975b2b3e9be
Paul <go2mailshan@gmail.com>
2015-04-17 13:34:03 +0530

    making the base of

--- event-office.js
+++ event-office.js
@@ -1,23 +1,28 @@
-var EventOffice = function  ($) {
-	var eventOffice ={
-		
-		trigger:function (eventName, extraParams) {
-			helpers.storeEvent(eventName,extraParams);
-			$.trigger(eventName,extraParams);
+var $EO = function  ($) {
+	
+	//prototype of $EO which will handle all the extended functions 
+	var EventOfficePrototype =function(){	
+		this.selector='html';	
+		this.trigger=function (eventName, params) {
+			var selector = $(this.selector);
+			console.log(selector);
 		}
 	};
 
-	var helpers={
-		storeEvent:function (eventName, extraParams) {
-			storage[eventName]={
-				name:eventName,
-				params:extraParams,
-				savedOn:new Date()
-			}
-		}
-	}
+	//replacement of $ for our custom work
+	var $EO=function (selectorName) {
+		return Object.create(Selector.prototype,{selector:selectorName});
+	};
 
-	var storage={
-	}
+	var Selector =function  (params) {
+		EventOfficePrototype.call(this);
+		this.selector = params.selector;
+	};
 
+	//making prototype
+	Selector.prototype = Object.create(EventOfficePrototype.prototype);
+
+
+
+	return $EO;
 }(jQuery)
