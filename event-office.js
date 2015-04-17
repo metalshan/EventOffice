5897bd3dd047f6c84547d241c212703a88df41ae
Paul <go2mailshan@gmail.com>
2015-04-17 17:08:21 +0530

    developping trigger

--- event-office.js
+++ event-office.js
@@ -4,16 +4,26 @@ var $EO = function  ($) {
 	var EventOfficePrototype =function(){	
 		this.selector='html';	
 		this.trigger=function (eventName, params) {
-			var selector = $(this.selector);
-			console.log(selector);
+			var selector = $(this.selector);			
+			helpers.storeEvent(this.selector, eventName, params);
+			selector.trigger(eventName, params);
+			console.log('event '+eventName+' is triggered on '+this.selector);
+		};
+		this.on=function (eventName, dynamicElement, callback) {
+			if(typeof dynamicElement === "function")
+				callback=dynamicElement;
+
+			var previousEventResponse = helpers.findFromStore(this.selector, eventName, dynamicElement); //return the previous response object if you have it
+			if(previousEventResponse){
+				setTimeout(function () {
+					callback.apply(this,previousEventResponse);
+				}.bind(this),20);
+				
+			}
 		}
 	};
 
-	//replacement of $ for our custom work
-	var $EO=function (selectorName) {
-		return Object.create(Selector.prototype,{selector:selectorName});
-	};
-
+	//
 	var Selector =function  (params) {
 		EventOfficePrototype.call(this);
 		this.selector = params.selector;
@@ -22,7 +32,27 @@ var $EO = function  ($) {
 	//making prototype
 	Selector.prototype = Object.create(EventOfficePrototype.prototype);
 
+	var helpers={
+		storeEvent:function (selectorName, eventName, params) {
+			Store[selectorName+'$EO$'+eventName] = {
+				selectorName:selectorName,
+				eventName:eventName,
+				params:params,
+				lastRaised: new Date()
+			}
+		},
 
+		findFromStore:function (selector, eventName, dynamicElement) {
+			return arguments;
+		}
 
+	};
+
+	var Store={}; //an object to store all raised events 
+
+	//replacement of $ for our custom work
+	var $EO=function (selectorName) {
+		return new Selector({selector:selectorName});
+	};
 	return $EO;
 }(jQuery)
