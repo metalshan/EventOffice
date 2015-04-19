1cd00be6442133c5aca407f33b4fc4f154b86f84
Paul <go2mailshan@gmail.com>
2015-04-19 20:47:07 +0530

    completing basic functionalities of post event listing without dynamicElement handling

--- event-office.js
+++ event-office.js
@@ -13,8 +13,17 @@ var $EO = function  ($) {
 		};
 		//custom on function for event listing
 		this.on=function (eventName, dynamicElement, callback) {
-			if(typeof dynamicElement === "function")
+			//register events normally
+			if(typeof dynamicElement !== "function")
+				$(this.selector).on(eventName, dynamicElement, $.proxy(callback,this));
+			else
+				$(this.selector).on(eventName, $.proxy(callback,this));	
+
+
+			if(typeof dynamicElement === "function"){
 				callback=dynamicElement;
+				dynamicElement=null;
+			}
 
 			var previousEventResponse = helpers.findFromStore(this.selector, eventName, dynamicElement); //return the previous response object if you have it
 			if(previousEventResponse){
@@ -23,16 +32,6 @@ var $EO = function  ($) {
 				}.bind(this),20);
 				
 			}
-
-			// //register events normally
-			// if(typeof dynamicElement !== "function"){
-			// 	$(this.selector).on(eventName, dynamicElement, $.proxy(callback,this));
-			// 	$(this.selector).on(eventName, dynamicElement, );
-			// }
-			// else{
-			// 	$(this.selector).on(eventName, $.proxy(callback,this));				
-			// 	$(this.selector).on(eventName, $.proxy(helpers.storeEventResponse,this));				
-			// }
 		}
 	};
 
@@ -50,7 +49,7 @@ var $EO = function  ($) {
 			var theEvent = arguments[0];
 			var eventName = theEvent.type;
 
-			Store.eventResponseStack.push({eventName:eventName, arguments:arguments});
+			Store.eventResponseStack.push({eventName:eventName, arguments:arguments, e: theEvent});
 		},
 
 		//check if EO already has an event listener to listen the event if not, create
@@ -62,7 +61,7 @@ var $EO = function  ($) {
 			else{
 				//the custon event listener just listens and shores in the eventResponse stack without any business logic
 				var eventListenerFunc = function () {
-					Store.saveEventResponse(arguments);
+					helpers.saveEventResponse.apply(this,arguments);
 				}
 
 				var newEventListenerObj = {
@@ -73,7 +72,7 @@ var $EO = function  ($) {
 				helpers.saveEvent(selectorName,eventName, newEventListenerObj);
 				
 				$(selectorName).on(eventName, newEventListenerObj.listenerFunc);
-				return Store.eventListeners[newEventId];
+				return newEventListenerObj;
 			}
 		},
 
@@ -83,8 +82,28 @@ var $EO = function  ($) {
 		},
 
 		saveEvent:function (selectorName,eventName, newEventListenerObj) {
-			Store.eventListeners[selectorName+"$EO$"+eventName] = newEventListenerObj;
+			newEventListenerObj.id = selectorName+"$EO$"+eventName; 
+			Store.eventListeners.push(newEventListenerObj);
 		},
+
+		findFromStore:function (selectorName, eventName, dynamicElement) {
+			var filteredEvents = $.grep(Store.eventResponseStack,function (eventObj, i) {
+				return eventObj.eventName === eventName;
+			}); 
+			var eventResponse = null;
+			for (var i = filteredEvents.length - 1; i >= 0; i--) {
+				var obj = filteredEvents[i];
+				//if(!dynamicElement){
+					//if same node or a child node, than it's true
+					if($(obj.e.target).is(selectorName) || $(selectorName).parent(obj.e.target).length){
+						eventResponse = obj.arguments;
+						return eventResponse;
+					}
+				//}
+			};;
+
+			return eventResponse;
+		}
 
 	};
 
